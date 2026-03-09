e1d09c33e663c6d8713b8807a1bff16d35ef766f
Aleksey Kubkovskiy <akubkovskiy@gmail.com>
2026-03-09 19:13:51 +0300

    fix: update coordinates to correct location 43.571307, 41.317629

--- frontend/site.config.js
+++ frontend/site.config.js
@@ -4,14 +4,14 @@ const config = {
   adminTelegram: process.env.NEXT_PUBLIC_ADMIN_TELEGRAM || "Alexey_kubkovskiy",
   phone: process.env.NEXT_PUBLIC_PHONE || "+79251279722",
   email: process.env.NEXT_PUBLIC_EMAIL || "teploarkhyz@gmail.com",
-  coords: { lat: "43.560731", lon: "41.284236" },
+  coords: { lat: "43.571307", lon: "41.317629" },
 };
 
 config.botUrl = `https://t.me/${config.botUsername}`;
 config.adminUrl = `https://t.me/${config.adminTelegram}`;
 config.phoneHref = `tel:${config.phone.replace(/[\s\-()]/g, "")}`;
 config.emailHref = `mailto:${config.email}`;
-config.yandexMapUrl = `https://yandex.ru/maps/?pt=${config.coords.lon},${config.coords.lat}&z=15&l=map`;
+config.yandexMapUrl = "https://yandex.com/maps/-/CPupUDor";
 config.googleMapUrl = `https://www.google.com/maps?q=${config.coords.lat},${config.coords.lon}`;
 config.gisMapUrl = `https://2gis.ru/geo/${config.coords.lon},${config.coords.lat}`;
 
